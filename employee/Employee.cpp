1e870122c87cfaffea0046c55e47acff984f33ad
Noah Melton <nmelton3@ut.utm.edu>
2025-04-17 16:15:12 -0500

    emp.h and .cpp done

--- employee/Employee.cpp
+++ employee/Employee.cpp
@@ -1,12 +1,41 @@
+/**
+ * @file Employee.cpp
+ * @author Noah Melton
+ * @date 2025-04-17
+ * @brief This is the implementation of the employee class
+ * 
+ * This is where we define how the funcitons behave
+ */
+
 #include "Employee.h"
 #include <iostream>
 
 using namespace std;
 
+
+/**
+ * The constructor for the employee class
+ *
+ * @pre This sets up the member variables of the employee class
+ * @post The member variables areae all initialized to -1
+ * 
+ */
 Employee::Employee() {
   ID = years = hoursWorked = hourlyRate = -1;
 }
 
+
+/**
+ * The paramaterized constructor for the employee class
+ *
+ * @param int ID the employee id
+ * @param int years the number of years that they have worked for the company
+ * @param double hourlyRate the amount the employee gets paid per hour
+ * @param float hoursWorked the number of hours that an employee has worked
+ * @pre This function takes the member variables and sets them equal to the values passed in the arguments section
+ * @post the member variables are set equal to whatever is in the parenthesis
+ * 
+ */
 Employee::Employee(int ID, int years, double hourlyRate, float hoursWorked) {
   this->ID = ID;
   this->years = years;
@@ -14,12 +43,30 @@ Employee::Employee(int ID, int years, double hourlyRate, float hoursWorked) {
   this->hoursWorked = hoursWorked;
 }
 
+
+/**
+ * This function prints out the employee info
+ *
+ * @pre A valid entry for an employee must already exist
+ * @return void This function doesn't return anything, it just prints out what is already in the employee object
+ * @post The details of the employee object is printed out
+ * 
+ */
 void Employee::print() {
   cout << "Printing information for employee " << ID << ":\n Years Employed: " << years
        << "\n Hourly Rate: " << hourlyRate << "\n Hours Worked: " << hoursWorked
        << endl;
 }
 
+
+/**
+ * This function is for celebrating x amount of years an employee works
+ *
+ * @pre An employee object must exist for us to be able to call this funciton
+ * @return void This funciton raises the employee
+ * @post This funciton raises the employee's hourly rate and congratulates them for their years of work
+ * 
+ */
 void Employee::anniversary() {
   years++;
   hourlyRate = hourlyRate + hourlyRate * .002;
@@ -27,6 +74,15 @@ void Employee::anniversary() {
        << endl;
 }
 
+
+/**
+ * This function calculates the amount of pay that an employee is owed
+ *
+ * @pre This function requires that an employee has already entered in the information about how long they have worked
+ * @return double This function returns the dollar amount that an employee is owed. 
+ * @post The dollar amount for the next paycheck is printed out
+ * 
+ */
 double Employee::calculatePay() {
   return hourlyRate * hoursWorked;
 }
--- employee/Employee.h
+++ employee/Employee.h
@@ -1,19 +1,83 @@
+/**
+ * @file Employee.h
+ * @author Noah Melton
+ * @date 2025-04-17
+ * @brief This is the header file for the employee class where we declare the funcitons of the class
+ * 
+ * This is where we declare, but do not implement, the functions in the employee class
+ */
+
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+
+/**
+ * The Employee class -- this is how the company keeps track of the info on their employees
+ *
+ * @class Employee Employee.h "employee/Employee.h"
+ * @brief This class keeps up with information related to the employee, such as their employee ID and how many years they have worked
+ *
+ */
 class Employee {
  private:
   int ID; //employee ID
-  int years;
+  int years; //how many years they have worked
   
  protected:
-  double hourlyRate;
-  float hoursWorked;
+  double hourlyRate;  //the hourly rate that the employee works for
+  float hoursWorked;  // how many hours that the employee has worked 
  public:
+
+/**
+ * This is the print function
+ *
+ * @pre An existing and populated instance of the employee class must exist
+ * @return virtual no return type, this function just prints out details
+ * @post The details of the employee class instance are printed out
+ * 
+ */
   virtual void print();
+
+/**
+ * This function calculates the pay that an employee is owed
+ *
+ * @pre The information (such as hourly rate and hours worked) needs to be entered already
+ * @return virtual No return type--this function only calculates the pay that the employee is owed
+ * @post The employee's pay is printed out
+ * 
+ */
   virtual double calculatePay();
+
+/**
+ * This function is for celebrating the anniversiary of an employee working at the company
+ *
+ * @pre N/A -- this function does not need anything to be true, it can be called whenever the user decides
+ * @return void this function doesn't return anything, it just adjusts the employee's pay and prints out a celebratory message. 
+ * @post The employee's pay is slightly bumped up and they are congratualed for their years of work
+ * 
+ */
   void anniversary();
+
+/**
+ * this is the default constructor
+ *
+ * @pre N/A
+ * @post the employee class is created with all of the member variables being set to -1
+ * 
+ */
   Employee();
+
+/**
+ * this is the parameterized constructor
+ *
+ * @param int ID this is the employee id
+ * @param int years the number of years that an employee has worked at the company
+ * @param double hourlyRate The hourly rate that the employee is paid at
+ * @param float hoursWorked the number of hours that the employee has worked in the pay period
+ * @pre N/A
+ * @post The member variables are set to whatever the user specifies in the constructor. 
+ * 
+ */
   Employee(int ID, int years, double hourlyRate, float hoursWorked);
 };
 
